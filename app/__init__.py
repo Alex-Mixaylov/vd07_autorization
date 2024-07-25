b5470bc5854efa610ac14822073614ae7299c643
Alex <guardamar81@gmail.com>
2024-07-25 22:54:43 +0400

    update init.py
    update init.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
+# Инициализация Flask приложения
 app = Flask(__name__)
+# Установка секретного ключа для сессий
 app.config['SECRET_KEY'] = 'your_secret_key'
+# Настройка базы данных
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+# Инициализация расширений Flask
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'login'  # Установка представления для страницы входа
 
-from app import routes
+from app import routes  # Импорт маршрутов
