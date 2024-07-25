0499579c33bfbd7a970fdef0ac7d2c5586c23e99
Alex <guardamar81@gmail.com>
2024-07-25 22:20:58 +0400

    2
    2

--- app/models.py
+++ app/models.py
@@ -1,11 +1,9 @@
-
-
 from app import db, login_manager
-from flask_login import UserMixin
+from flask_login import UserMixin # Этот класс даёт возможность работать с пользователем
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return User.query.get(int(user_id)) # Эта строчка будет отправлять в БД запрос для поиска определённого юзера по его ID
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
