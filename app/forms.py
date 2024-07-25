f30ce9917117e48b88f6660cc5fad5c62941d128
Alex <guardamar81@gmail.com>
2024-07-25 23:20:11 +0400

    update forms.py
    update forms.py

--- app/forms.py
+++ app/forms.py
@@ -20,7 +20,6 @@ class RegistrationForm(FlaskForm):
         if email:
             raise ValidationError('Такая почта уже используется')
 
-
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -30,16 +29,17 @@ class LoginForm(FlaskForm):
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=35)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    old_password = PasswordField('Old Password', validators=[DataRequired()])
+    new_password = PasswordField('New Password', validators=[Length(min=6)])
+    confirm_new_password = PasswordField('Confirm New Password', validators=[EqualTo('new_password')])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
-        if user:
+        if user and user != current_user:
             raise ValidationError('Такое имя уже существует')
 
     def validate_email(self, email):
         email = User.query.filter_by(email=email.data).first()
-        if email:
+        if email and email != current_user:
             raise ValidationError('Такая почта уже используется')
